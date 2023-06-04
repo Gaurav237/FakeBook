f84edde8cdff50000b77dbafdfe8d6d5baef4066
Gaurav237 <gktiwary237@gmail.com>
2023-06-05 01:00:45 +0530

    added another user router & controller

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
 
+// for other routers other than index.js i.e. home router
+router.use('/users', require('./users.js'));
+
+// for any other routes, access using middleware
+// app.use('/routerName', require('./routerFile'));
+
 module.exports = router;
