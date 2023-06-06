1758bc02b188c4c3465af4cbfd55f1f69cb110cf
Gaurav237 <gktiwary237@gmail.com>
2023-06-07 00:09:13 +0530

    setup mongo store (connect-mongo)

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 
+// setting up mongo store
+const MongoStore = require('connect-mongo')(session);
+
 app.use(express.urlencoded({extended: true}));
 
 app.use(cookieParser());
@@ -20,14 +23,24 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 // express session
+// mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'FakeBook',
-    secret: 'mySecretKey',  //will change it later
+    secret: 'mySecretKey',  //will change it later 
     saveUninitialized: false,
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)  // maximum age(expiry) of the session cookie in milliseconds
-    }
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,  // mongoose.connection
+            autoRemove: 'disabled'
+        },
+        function (err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
