3d528439e38609853cff643fa7c0db7378e63494
Gaurav237 <gktiwary237@gmail.com>
2023-06-07 20:31:43 +0530

    displaying comments on each post (with User)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,6 +4,16 @@ module.exports.home = function(req, res){
         
     Post.find({})
         .populate('user', 'name')  // only name & _id will be populated
+
+        // NESTED population =>   to populate the comments field of a document.
+        // Along with populating the comments field, the user field of each comment is also populated.
+        .populate({  
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+
         .then(posts => {
             return res.render('home', {
                 title: 'Home',
