0414e5c1ba680ef024df053c228b1edb0ae418fb
Gaurav237 <gktiwary237@gmail.com>
2023-06-07 19:06:52 +0530

    authenticate on Creating Post (2 level)

--- routes/posts.js
+++ routes/posts.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const postsController = require('../controllers/posts_controller');
+const passport = require('passport');
 
-router.post('/create', postsController.create);
+// controller level authentication added for posting.
+router.post('/create', passport.checkAuthentication ,postsController.create);
 
 
 module.exports = router;
