6eb0bb86585885625465dca7001546f3e185b6d7
Gaurav237 <gktiwary237@gmail.com>
2023-06-12 01:43:09 +0530

    authentication and authorizatoion using Token(JWT)

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -3,8 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const postsApi = require('../../../controllers/api/v1/posts_api');
+const passport = require('passport');
 
 router.get('/', postsApi.index);
 
+// 'jwt' => used to authenticate, && don't store session cookies
+router.delete('/:id', passport.authenticate('jwt', {session: false}), postsApi.destroy);
 
 module.exports = router;
